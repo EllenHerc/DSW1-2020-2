d8f8eb13a4add04d4e568e4cf7f2c6eb9bfdf016
Ellen Cristina Herculano <ellencristina125@gmail.com>
2021-05-29 18:45:09 -0300

    correção filtro cidades na lista de imóveis

--- CompraVendaImoveis/src/main/java/br/ufscar/dc/dsw/controller/CidadeController.java
+++ CompraVendaImoveis/src/main/java/br/ufscar/dc/dsw/controller/CidadeController.java
@@ -21,6 +21,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import java.io.PrintWriter;
 
 /**
  *
@@ -31,28 +32,31 @@ public class CidadeController extends HttpServlet{
     
     private static final long serialVersionUID = 1L;
 
+    protected List<String> buscaCidade(String term) throws ClassNotFoundException, SQLException{
+        
+        List<CidadeBean> cidades = new CidadeDao().getAll();
+                List<String> listaCidades = new ArrayList<>();
+        if (term.length()>0){
+            for (CidadeBean cidade : cidades) {
+                if (cidade.getNome().toLowerCase().contains(term.toLowerCase())) {
+                    listaCidades.add(cidade.getNome()+" - "+cidade.getUf());          
+                }
+             }
+        }
+        return listaCidades;
+    }
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException, ClassNotFoundException, SQLException {
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
 
-        String nome = request.getParameter("term");
-
         Gson gsonBuilder = new GsonBuilder().create();
-        List<String> cidades = new ArrayList<>();
-        if (nome.length()>0){
-            for (CidadeBean cidade : new CidadeDao().getAll()) {
-                cidades.add(cidade.getNome()+" - "+cidade.getUf());          
-            }
-        }else{
-            for (CidadeBean cidade : new CidadeDao().getByName(nome)) {
-                cidades.add(cidade.getNome()+" - "+cidade.getUf());          
-            }
-        }
-        System.out.println(gsonBuilder.toJson(cidades));
-        response.getWriter().write(gsonBuilder.toJson(cidades));
         
+        PrintWriter out = response.getWriter();
+	out.print(gsonBuilder.toJson(buscaCidade(request.getParameter("term"))));
+	out.flush();
+	out.close();
     }
     
     
