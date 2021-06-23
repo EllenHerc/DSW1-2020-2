44a11b31dd0224dfa16974254b1a4b532037d4ff
Ellen Cristina Herculano <ellencristina125@gmail.com>
2021-06-23 06:09:04 -0300

    Update bd.sql

--- BancoDeDadosT2/bd.sql
+++ BancoDeDadosT2/bd.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE  IF NOT EXISTS `bancodedadost2` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `bancodedadost2`;
--- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `compravendaimoveist2` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `compravendaimoveist2`;
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
--- Host: localhost    Database: bancodedadost2
+-- Host: localhost    Database: compravendaimoveist2
 -- ------------------------------------------------------
--- Server version	8.0.25
+-- Server version	8.0.22
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,159 @@ USE `bancodedadost2`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cidade`
+--
+
+DROP TABLE IF EXISTS `cidade`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cidade` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nome` varchar(20) NOT NULL,
+  `uf` varchar(2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `cpf` varchar(14) NOT NULL,
+  `nascimento` date NOT NULL,
+  `nome` varchar(256) NOT NULL,
+  `sexo` varchar(10) NOT NULL,
+  `telefone` varchar(17) NOT NULL,
+  `usuario_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK4tbh39k0ug33429sademagg6o` (`usuario_id`),
+  CONSTRAINT `FK4tbh39k0ug33429sademagg6o` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `foto`
+--
+
+DROP TABLE IF EXISTS `foto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `foto` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `url` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `imobiliaria`
+--
+
+DROP TABLE IF EXISTS `imobiliaria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `imobiliaria` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `cnpj` varchar(18) NOT NULL,
+  `descricao` varchar(256) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `usuario_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_n0pwhw8x6i8khn13ar7xf7w35` (`cnpj`),
+  KEY `FKnwho6y9yc1jfifmmtfn0jvjla` (`usuario_id`),
+  CONSTRAINT `FKnwho6y9yc1jfifmmtfn0jvjla` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `imovel`
+--
+
+DROP TABLE IF EXISTS `imovel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `imovel` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `bairro` varchar(100) NOT NULL,
+  `cep` varchar(11) NOT NULL,
+  `descricao` varchar(256) NOT NULL,
+  `logradouro` varchar(100) NOT NULL,
+  `numero` int NOT NULL,
+  `valor` float NOT NULL,
+  `cidade_id` bigint NOT NULL,
+  `imobiliaria_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK2874ewv814m4jhut2iiyv0k9w` (`cidade_id`),
+  KEY `FKdj2sm24kfixxylm7urwa3n92p` (`imobiliaria_id`),
+  CONSTRAINT `FK2874ewv814m4jhut2iiyv0k9w` FOREIGN KEY (`cidade_id`) REFERENCES `cidade` (`id`),
+  CONSTRAINT `FKdj2sm24kfixxylm7urwa3n92p` FOREIGN KEY (`imobiliaria_id`) REFERENCES `imobiliaria` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `imovel_fotos`
+--
+
+DROP TABLE IF EXISTS `imovel_fotos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `imovel_fotos` (
+  `Imovel_id` bigint NOT NULL,
+  `fotos_id` bigint NOT NULL,
+  UNIQUE KEY `UK_2mf797i530vnhn53u6yb1jx41` (`fotos_id`),
+  KEY `FK8suo098gmgn1xhha7unux9kp9` (`Imovel_id`),
+  CONSTRAINT `FK8suo098gmgn1xhha7unux9kp9` FOREIGN KEY (`Imovel_id`) REFERENCES `imovel` (`id`),
+  CONSTRAINT `FKflgawc32vrwf866obygf3r9yd` FOREIGN KEY (`fotos_id`) REFERENCES `foto` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `proposta`
+--
+
+DROP TABLE IF EXISTS `proposta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `proposta` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `dataemissao` date NOT NULL,
+  `pagamento` varchar(256) NOT NULL,
+  `status` varchar(11) NOT NULL,
+  `valor` float NOT NULL,
+  `cliente_id` bigint NOT NULL,
+  `imovel_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKbn34rsnpnciyhkl7sjbfrbcp7` (`cliente_id`),
+  KEY `FKom65c42p1ech7jhu0yj9rkqa0` (`imovel_id`),
+  CONSTRAINT `FKbn34rsnpnciyhkl7sjbfrbcp7` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`),
+  CONSTRAINT `FKom65c42p1ech7jhu0yj9rkqa0` FOREIGN KEY (`imovel_id`) REFERENCES `imovel` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `enabled` bit(1) NOT NULL,
+  `papel` varchar(16) NOT NULL,
+  `senha` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_4tdehxj7dh8ghfc68kbwbsbll` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -26,4 +179,5 @@ USE `bancodedadost2`;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-22 22:26:56
+-- Dump completed on 2021-06-23  6:08:17
+
